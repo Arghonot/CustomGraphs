b9004854eb8acfa4b6972a490b7ec9ee450c42ab
Rivemale Loïck <lo.rivemale@gmail.com>
2020-05-04 17:43:25 +0200

    Added the basis for subgraphs management.

---
+++ Script/SubGraph/SubGraph.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XNode;
+
+namespace BT
+{
+    [CreateNodeMenu("MYBT/SubGraph")]
+    public class SubGraph : Node
+    {
+        public TestGraph TargetGraph;
+    }
+}
---
+++ Script/SubGraph/SubGraphEditor.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using static XNodeEditor.NodeEditor;
+
+namespace BTEditor
+{
+    [CustomNodeEditor(typeof(BT.SubGraph))]
+    public class SubGraphEditor : XNodeEditor.NodeEditor
+    {
+        public override void OnBodyGUI()
+        {
+            BT.SubGraph SubGraph = target as BT.SubGraph;
+            BT.TestGraph owngraph = (BT.TestGraph)SubGraph.graph;
+
+            SubGraph.TargetGraph = (BT.TestGraph)EditorGUILayout.ObjectField(
+                "Sub graph ",
+                SubGraph.TargetGraph,
+                typeof(BT.TestGraph),
+                false);
+
+            if (SubGraph.TargetGraph == owngraph)
+            {
+                SubGraph.TargetGraph = null;
+                Debug.LogError("[SubGraph Node Error] : Can't feed self graph to a subgraph node.");
+            }
+
+        }
+    }
+}
