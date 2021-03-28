52bd0a33af751d59ff996ee5a2b6246d0bffce36
Arghonot <lo.rivemale@gmail.com>
2021-03-28 20:27:01 +0100

    added a new color for a new type of node.

--- Script/Nodes/Base/Root.cs
+++ Script/Nodes/Base/Root.cs
@@ -3,7 +3,8 @@
     public static class ColorProfile
     {
         //public const string Root = "#f1faee";
-        public const string Root = "#a8dadc";
+        //public const string Root = "#a8dadc";
+        public const string Root = "#E9C46A";
         //public const string Branch = "#457b9d";
         public const string Branch = "#48a9a6";
         //public const string Leaf = "#a8dadc";
@@ -11,6 +12,7 @@
         public const string Blackboard = "#1d3557";
         public const string other1 = "#e63946";
         public const string Debug = "#393D3F";
+        public const string FlatBlue = "#2E5EAA";
     }
 
     [NodeTint(ColorProfile.Root)]
