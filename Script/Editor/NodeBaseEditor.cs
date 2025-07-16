addb1cf68d04ce7448fcd2044966883cbc878b8c
Arghonaut <lo.rivemale@gmail.com>
2025-07-16 13:25:04 +0700

    Added a new way to handle inputs without having to force an concrete type to be used in a graph (eg my Libnoise's use of ModuleBase which was abstract).

---
+++ Script/Editor/NodeBaseEditor.cs
@@ -0,0 +1,53 @@
+﻿using UnityEditor;
+using XNode;
+using XNodeEditor;
+using System.Reflection;
+using static XNode.Node;
+using UnityEngine;
+using System;
+
+
+namespace CustomGraph
+{
+    [CustomNodeEditor(typeof(NodeBase))]
+    public class NodeBaseEditor : NodeEditor
+    {
+        public override void OnBodyGUI()
+        {
+            serializedObject.Update();
+
+            // Let XNode handle all ports/fields normally
+            base.OnBodyGUI();
+            DrawNonInstantiableTypeInputPorts(target);
+
+            serializedObject.ApplyModifiedProperties();
+        }
+
+        public static void DrawNonInstantiableTypeInputPorts(object target)
+        {
+            NodeBase node = target as NodeBase;
+
+            // Check for problematic [Input] fields
+            foreach (FieldInfo field in node.GetType().GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance))
+            {
+                var inputAttr = field.GetCustomAttribute<InputAttribute>();
+                if (inputAttr == null) continue;
+
+                // Get field type
+                Type fieldType = field.FieldType;
+
+                // Check if abstract/interface/generic
+                bool isProblematic = fieldType.IsAbstract || fieldType.IsInterface;
+
+                if (isProblematic)
+                {
+                    NodePort port = node.GetPort(field.Name);
+                    if (port != null)
+                    {
+                        NodeEditorGUILayout.PortField(new GUIContent(ObjectNames.NicifyVariableName(field.Name)), port);
+                    }
+                }
+            }
+        }
+    }
+}
